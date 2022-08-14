3899cc1134a7f105bf7de8ec9a56c56598af2b91
codingepaduli <padulodeveloper@protonmail.com>
2022-08-14 18:57:07 +0200

    web - added static contents about color mixing

---
+++ static/static/coding/web/p5js/colors_and_styles_rgb.js
@@ -0,0 +1,25 @@
+var canvas;
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.parent('rgb');
+}
+
+function draw() {
+  // Note: Don't apply background
+
+  // Note: SCREEN seems to be the blending mode for additive color mixing
+  blendMode(SCREEN);
+
+  fill(255, 0, 0);
+  circle(150, 100, 200);
+
+  fill(0, 255, 0);
+  circle(100, 200, 200);
+  
+  fill(0, 0, 255);
+  circle(200, 200, 200);
+
+  // FIXME: can't save in PNG when blending mode is SCREEN 
+  saveCanvas(canvas, 'colors_and_styles_rgb', 'png');
+  noLoop();
+}
