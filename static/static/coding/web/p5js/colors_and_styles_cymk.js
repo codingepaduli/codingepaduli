4f65f6ac9a2a71989bdb2f3588d4bbf64cbfd6e5
codingepaduli <padulodeveloper@protonmail.com>
2022-08-14 20:04:57 +0200

    web - added static contents about color mixing

---
+++ static/static/coding/web/p5js/colors_and_styles_cymk.js
@@ -0,0 +1,24 @@
+var canvas;
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.parent('cymk');
+}
+
+function draw() {
+  background(255, 255, 255);
+
+  blendMode(MULTIPLY);
+
+  fill(0, 255, 255);
+  circle(150, 100, 200);
+
+  fill(255, 255, 0);
+  circle(100, 200, 200);
+  
+  fill(255, 0, 255);
+  circle(200, 200, 200);
+
+  // FIXME: can't save in PNG when blending mode is SCREEN 
+  saveCanvas(canvas, 'colors_and_styles_cymk', 'png');
+  noLoop();
+}
