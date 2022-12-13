24192712a92a255bbea7e7540fca03f003513e04
codingepaduli <padulodeveloper@protonmail.com>
2022-12-13 13:45:12 +0100

    Minor Fix - Fixed articles against eslint linter

--- static/static/coding/web/javascript/ajax.js
+++ static/static/coding/web/javascript/ajax.js
@@ -5,7 +5,7 @@ const options = {
   cache: 'no-cache'
 };
 
-/* eslint-disable-next-line no-unused-vars */
+/* eslint-disable-next-line no-unused-vars jsdoc/require-jsdoc */
 function callGetJSON() {
     let source = document.querySelector('#loadImage');
 
