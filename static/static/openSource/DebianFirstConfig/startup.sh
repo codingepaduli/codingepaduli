8beb9268980d0800fb9a3a4d9fb70654123254b0
codingepaduli <padulodeveloper@protonmail.com>
2023-08-10 23:44:42 +0200

    OpenSource - Updated post about first Debian GNU/Linux configuration - added user creation

--- static/static/openSource/DebianFirstConfig/startup.sh
+++ static/static/openSource/DebianFirstConfig/startup.sh
@@ -4,10 +4,14 @@
 # ./startup.sh > >(tee -a stdout.log) 2> >(tee -a stderr.log >&2)
 
 # Install all the apps
-. apt-install-app.sh
-. docker-installedApp.sh
-. flatpak-installAllScript.sh
-. npm-installedApp.sh
+# . apt-install-app.sh
+# . docker-installedApp.sh
+# . flatpak-installAllScript.sh
+# . npm-installedApp.sh
+# 
+# # Remove unwanted apps
+# . apt-remove-app.sh
 
-# Remove unwanted apps
-. apt-remove-app.sh
+# Create users and groups
+. create-users.sh
+
