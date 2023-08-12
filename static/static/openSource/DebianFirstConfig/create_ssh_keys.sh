ef10bc67e57b54126d2e2dd24e58f9ac1707e7cc
codingepaduli <padulodeveloper@protonmail.com>
2023-08-12 07:58:50 +0200

    OpenSource - Updated post about first Debian GNU/Linux configuration - added SSH key generation

---
+++ static/static/openSource/DebianFirstConfig/create_ssh_keys.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# -N empty      password
+# -q quiet      mode
+# <<< $'y\n'    answer 'File exists. Overwrite?' with 'yes' and '\n' (newline)
+# >/dev/null    2>&1 hide output
+ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -C "padulodeveloper@protonmail.com" -N '' -q <<< $'y\n' >/dev/null 2>&1
+
+eval "$(ssh-agent -s)"
+
+ssh-add ~/.ssh/id_ed25519
+
+# Set the key as testable
+#  "\(" is an escapes for "(" and "\)" is an escapes for ")" 
+# () is a match (may need to be escaped \(\), it will be stored
+# (.*) (.*) (.*) are three matches, they will be stored as \1 \2 \3
+# in the replace expression, \3 and \2 will be replaced by the captured maches
+cat ~/.ssh/id_ed25519.pub | sed 's/\(.*\) \(.*\) \(.*\)/\3 ssh-ed25519 \2/g' > ~/.ssh/allowed_signers
+
+echo "Please update the signing key to the git hosting service"
+
+# Remember to add a password
+# ssh-keygen -p -f ~/.ssh/id_ed25519
--- static/static/openSource/DebianFirstConfig/startup.sh
+++ static/static/openSource/DebianFirstConfig/startup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-# Per visualizzare e salvare l'output di messaggi ed errore nei file di log
-# ./startup.sh > >(tee -a stdout.log) 2> >(tee -a stderr.log >&2)
+# Checkout del repository
+# git clone https://github.com/codingepaduli/codingepaduli.git
+
+# Cartella script
+# cd codingepaduli/static/static/openSource/DebianFirstConfig/
+
+# Avvia lo script
+#     Per visualizzare e salvare l'output di messaggi ed errore nei file di log
+#     ./startup.sh > >(tee -a stdout.log) 2> >(tee -a stderr.log >&2)
+
 
 # Install all the apps
 # . apt-install-app.sh
@@ -15,3 +23,7 @@
 # Create users and groups
 . create-users.sh
 
+# Create SSH keys
+cp create_ssh_keys.sh /home/io/create_ssh_keys.sh
+chown io:casa /home/io/create_ssh_keys.sh
+su -c '/home/io/create_ssh_keys.sh' io
