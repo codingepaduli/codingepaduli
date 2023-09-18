4fc208cc1ae2cda2d210ae5015072fb08fe1b9d9
codingepaduli <padulodeveloper@protonmail.com>
2023-09-18 18:22:12 +0200

    OpenSource - Updated post about first Debian GNU/Linux configuration - updated software management tools

--- static/static/openSource/DebianFirstConfig/root-startup.sh
+++ static/static/openSource/DebianFirstConfig/root-startup.sh
@@ -13,6 +13,12 @@
 # Avvia lo script e salva l'output di messaggi ed errori nei file di log
 #    ./root-startup.sh > >(tee -a stdout.log) 2> >(tee -a stderr.log >&2)
 
+# Imposta il log degli errori e l'uscita in caso di errore
+set -eE  # same as: `set -o errexit -o errtrace`
+
+# Log per errori
+trap 'printf "\e[31m Exit status: %s. \n Line: %s -- %s \n Line: %s -- %s \n Line: %s -- %s \n \n Error: %s \n Script or function: %s \e[m \n" "$?" "${BASH_LINENO[0]}" "${BASH_SOURCE[0]}" "${BASH_LINENO[1]}" "${BASH_SOURCE[1]}" "${BASH_LINENO[2]}" "${BASH_SOURCE[2]}" "${BASH_COMMAND[*]}" "${FUNCNAME[*]:-$(caller)}"  ' ERR
+
 DISTRO="debian12"
 
 # Install all the apps
