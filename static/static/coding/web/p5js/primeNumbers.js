0cd2bff35ae1032a6cb650d3ef37de3c23127cc7
codingepaduli <padulodeveloper@protonmail.com>
2024-07-23 13:01:54 +0200

    Minor Fix - Fixed articles against eslint linter

--- static/static/coding/web/p5js/primeNumbers.js
+++ static/static/coding/web/p5js/primeNumbers.js
@@ -120,7 +120,7 @@ function log10(bigint) {
   if (bigint < 0) return NaN;
   const s = bigint.toString(10);
 
-  return s.length + Math.log10("0." + s.substring(0, 15))
+  return s.length + Math.log10("0." + s.substring(0, 15));
 }
 
 function log(bigint) {
