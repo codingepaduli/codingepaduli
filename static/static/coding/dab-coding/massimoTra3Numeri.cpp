50a3a5ef661c53086fa594f65b644081aadfcf4b
codingepaduli <padulodeveloper@protonmail.com>
2025-06-02 23:25:37 +0200

    Minor Fix - Fix code indentation

--- static/static/coding/dab-coding/massimoTra3Numeri.cpp
+++ static/static/coding/dab-coding/massimoTra3Numeri.cpp
@@ -14,36 +14,36 @@ using namespace std;
 
 int main()
 {
-    // variabili input
-	int a,b,c;
-	
-	// variabili output
-	int max;
-	
-	// Leggo input dall'utente
-	cout<<"inserisci il primo numero:";
-	cin>>a;
-	cout<<"inserisci il secondo numero:";
-	cin>>b;
-	cout<<"inserisci il terzo numero:";
-	cin>>c;
-	
-	// Trovo il massimo
-	if(a>b)
-	{
-		max=a;
-	}
-	else 
-	{
-		max=b;
-	}
-	
-	if(c>max)
-	{
-		max=c;
-	}
-	
-	// Output del massimo
-	cout<<"il numero max dei tre numeri �:"<<max;
-	return 0;	
+  // variabili input
+  int a,b,c;
+
+  // variabili output
+  int max;
+
+  // Leggo input dall'utente
+  cout<<"inserisci il primo numero:";
+  cin>>a;
+  cout<<"inserisci il secondo numero:";
+  cin>>b;
+  cout<<"inserisci il terzo numero:";
+  cin>>c;
+
+  // Trovo il massimo
+  if(a>b)
+  {
+    max=a;
+  }
+  else 
+  {
+    max=b;
+  }
+
+  if(c>max)
+  {
+    max=c;
+  }
+
+  // Output del massimo
+  cout<<"il numero max dei tre numeri e':"<<max;
+  return 0;	
 }
