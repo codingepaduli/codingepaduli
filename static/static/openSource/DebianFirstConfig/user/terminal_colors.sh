e01b1b6ef3da723a3bf72fca9b2ae9b90e804275
codingepaduli <padulodeveloper@protonmail.com>
2025-07-02 14:42:35 +0200

    OpenSource - Updated post about first Debian GNU/Linux configuration - added bash alias for print with colors

--- static/static/openSource/DebianFirstConfig/user/terminal_colors.sh
+++ static/static/openSource/DebianFirstConfig/user/terminal_colors.sh
@@ -1,4 +1,4 @@
-# Colori:
+# Colori e formattazione:
 # Il terminale può supportare differenti palette di colore
 # - ANSI 8 bit (solo 7 colori di testo e 7 di sfondo, piu formattazione grassetto, corsivo, ...)
 # - 256 colori
@@ -10,11 +10,67 @@
 # - xterm-256color
 # - xterm-truecolor
 #
-# Il reset allo stile predefinito è reset_code="\033[0m"; Per applicare uno stile dei colori si usa:
+# ANSI escape sequences are a standard for in-band signaling to control 
+# cursor location, color, font styling, and other options on video text
+# terminals and terminal emulators
+#
+# Una sequenza "Ansi escape", detta anche CSI (Control Sequence Introducer), nella 
+# tabella ASCII è chiamata ESC, che ha valore \x1b[, \e, oppure \033 serve ad
+# indicare al terminale di eseguire un comando per muovere il cursore, 
+# applicare uno stile o utilizzare un colore
+# 
+# Dopo una sequenza "Ansi escape" si può invocare una funzione. 
+# La sintassi 0;1;34m la si può leggere come una chiamata alla 
+# funzione m(0,1,34), il nome della funzione è indicato dopo i 
+# parametri, che si separano tra loro con un punto e virgola. 
+# La sequenza \033A è semplicemente A().
+#
+# Le funzioni disponibili per spostare il cursore sono:
+# A(n)  Cursor Up - Move cursor up by n
+# B(n)  Cursor Down - Move cursor down by n
+# C(n)  Cursor Forward - Move cursor forward by n
+# D(n)  Cursor Back - Move cursor back by n
+# E(n)  Cursor Next Line - Move cursor to the beginning of the line n lines down
+# F(n)  Cursor Previous Line - Move cursor to the beginning of the line n lines up
+# G(n)  Cursor Horizontal Absolute - Move cursor to the the column n within the current row
+# H(n,m) Cursor Position - Move cursor to row n, column m, counting from the top left corner
+# J(n)  Erase in Display - Clear part of the screen. 0, 1, 2, and 3 have various specific functions
+# K(n)  Erase in Line - Clear part of the line. 0, 1, and 2 have various specific functions
+# S(n)  Scroll Up Scroll window up by n lines
+# T(n)  Scroll Down Scroll window down by n lines
+# s()  Save Cursor Position - Save current cursor position for use with u
+# u()  Restore Cursor Position -  Set cursor back to position last saved by s
+#
+# Quindi \x1b[1;3H invoca H(1,3) e sposta il cursore alla riga 1, colonna 3
+#
+# La funzione m() serve invece ad impostare i colori e la formattazione carattere, 
+# che nel terminale è detta SGR (Select Graphics Rendition).
+#
+# I valori che può utilizzare sono:
+#
+# 0             Reset: turn off all attributes
+# 1             Bold (or bright, it’s up to the terminal and the user config to some extent)
+# 3             Italic
+# 4             Underline
+# 30–37         Set text colour from the basic colour palette of 0–7
+# 38;5;n        Set text colour to index n in a 256-colour palette (e.g. \x1b[38;5;34m)
+# 38;2;r;g;b    Set text colour to an RGB value (e.g. \x1b[38;2;255;255;0m)
+# 40–47         Set background colour
+# 48;5;n        Set background colour to index n in a 256-colour palette
+# 48;2;r;g;b    Set background colour to an RGB value
+# 90–97         Set text colour from the bright colour palette of 0–7
+# 100–107       Set background colour from the bright colour palette of 0–7
+
+# Il reset allo stile predefinito è la funzione m(0) che si invoca con "\033[0m"
+# La formattazione carattere si può applicare in un solo colpo, concatenando gli 
+# stili da applicare. Se voglio applicare 1, 3, 4, allora invoco m(1, 3, 4), 
+# che diventa quindi "\033[1;3;4m"
+
+# Scrivere un testo formattato e colorato diventa quindi:
 # text_colored="\033[${background_code};${foreground_code};${formatting_code};${formatting_code}m  TEXT  ${reset_code}"
 
 # Assegnazione dei colori e dei loro codici
-foregroundAnsiColors=(
+ansiColors=(
     [30]="black"
     [31]="red"
     [32]="green"
@@ -23,17 +79,33 @@ foregroundAnsiColors=(
     [35]="magenta"
     [36]="cyan"
     [37]="white"
-)
 
-backgroundAnsiColors=(
-    [40]="black"
-    [41]="red"
-    [42]="green"
-    [43]="yellow"
-    [44]="blue"
-    [45]="magenta"
-    [46]="cyan"
-    [47]="white"
+    [40]="background black"
+    [41]="background red"
+    [42]="background green"
+    [43]="background yellow"
+    [44]="background blue"
+    [45]="background magenta"
+    [46]="background cyan"
+    [47]="background white"
+
+    [90]="bright black"
+    [91]="bright red"
+    [92]="bright green"
+    [93]="bright yellow"
+    [94]="bright blue"
+    [95]="bright magenta"
+    [96]="bright cyan"
+    [97]="bright white"
+
+    [100]="bright background black"
+    [101]="bright background red"
+    [102]="bright background green"
+    [103]="bright background yellow"
+    [104]="bright background blue"
+    [105]="bright background magenta"
+    [106]="bright background cyan"
+    [107]="bright background white"
 )
 
 formatting=(
@@ -49,6 +121,7 @@ formatting=(
     [9]="strikethrough"     # Soprascritto
 )
 
+# FIX-ME https://hexdocs.pm/color_palette/ansi_color_codes.html
 colors256=(
     [0]="black"
     [1]="red"
@@ -311,6 +384,7 @@ colors256=(
 ## Run: get256ColorIndexByName "blue"
 get256ColorIndexByName() {
     local color_name="$1"
+
     for i in "${!colors256[@]}"; do
         if [[ "${colors256[$i]}" == "$color_name" ]]; then
             echo "$i"
@@ -335,11 +409,11 @@ get256ColorNameByIndex() {
     return 0
 }
 
-## Run: getAnsiForegroundColorIndexByName "blue"
-getAnsiForegroundColorIndexByName() {
+## Run: getAnsiColorIndexByName "blue"
+getAnsiColorIndexByName() {
     local color_name="$1"
-    for i in "${!foregroundAnsiColors[@]}"; do
-        if [[ "${foregroundAnsiColors[$i]}" == "$color_name" ]]; then
+    for i in "${!ansiColors[@]}"; do
+        if [[ "${ansiColors[$i]}" == "$color_name" ]]; then
             echo "$i"
             return 0 # OK
         fi
@@ -348,26 +422,25 @@ getAnsiForegroundColorIndexByName() {
     return 2 # error
 }
 
-## Run: getAnsiForegroundColorNameByIndex "123"
-getAnsiForegroundColorNameByIndex() {
+## Run: getAnsiColorNameByIndex "123"
+getAnsiColorNameByIndex() {
     local code=$1
 
     # Controlla se il valore è vuoto
-    if [[ -z "${foregroundAnsiColors[$code]}" ]]; then
+    if [[ -z "${ansiColors[$code]}" ]]; then
         echo ""
         return 2 # error
     fi
     
-    echo "${foregroundAnsiColors[$code]}"
+    echo "${ansiColors[$code]}"
     return 0
 }
 
-
-## Run: getAnsiBackgroundColorIndexByName "blue"
-getAnsiBackgroundColorIndexByName() {
-    local color_name="$1"
-    for i in "${!backgroundAnsiColors[@]}"; do
-        if [[ "${backgroundAnsiColors[$i]}" == "$color_name" ]]; then
+## Run: getFormattingCodeByName "bold"
+getFormattingCodeByName() {
+    local formatting_name="$1"
+    for i in "${!formatting[@]}"; do
+        if [[ "${formatting[$i]}" == "$formatting_name" ]]; then
             echo "$i"
             return 0 # OK
         fi
@@ -376,16 +449,146 @@ getAnsiBackgroundColorIndexByName() {
     return 2 # error
 }
 
-## Run: getAnsiBackgroundColorNameByIndex "123"
-getAnsiBackgroundColorNameByIndex() {
-    local code=$1
+# Get the formatting code by more names
+## Run: getFormattingCodeByNames "bold" "dim" "blink"
+getFormattingCodeByNames() {
+    local formatting_names=("$@")
+    local formatting_codes=""
 
-    # Controlla se il valore è vuoto
-    if [[ -z "${backgroundAnsiColors[$code]}" ]]; then
-        echo ""
-        return 2 # error
-    fi
-    
-    echo "${backgroundAnsiColors[$code]}"
-    return 0
+    for paramName in "${formatting_names[@]}"; do
+        local param_code=$(getFormattingCodeByName "$paramName")
+        if [[ -n "${param_code}" ]]; then
+
+            # split elements by ";"
+            if [[ -n "${formatting_codes}" ]]; then
+                formatting_codes+=";"
+            fi
+
+            # adds element after ";"
+            formatting_codes+="${param_code}"
+        fi
+    done
+
+    echo "$formatting_codes"
 }
+
+# Funzione per selezionare il tipo di colore
+# getColorByName "sea green"
+getColorByName() {
+     # Parametri
+    local colorName="$1"
+
+    if [[ "$TERM" == *"truecolor"* || "$TERM" == *"24bit"* ]]; then
+        # True Color (24 bit) - No color names
+        echo ""
+    elif [[ "$TERM" == *"256color"* ]]; then
+        # ANSI 256 colors
+        echo "$(get256ColorIndexByName "$colorName")"
+    elif [[ "$TERM" == *"color"* ]]; then
+        # ANSI 8 colors
+        echo "$(getAnsiColorIndexByName "$colorName")"
+    else
+        # No color
+        echo ""
+    fi
+}
+
+# Apply a style by code
+## Run applyStyleByCodes "255:255;255" "255:255;255" "1" 
+applyStyleByCodes() {
+    # If $NO_COLOR not defined or empty
+    if [[ ! -v $NO_COLOR || -z "${!NO_COLOR}" ]]; then
+        # Parametri
+        local foreground_code=$1
+        local background_color=$2
+        local formatting_codes=$3
+
+        echo -ne "\033[38;5;${foreground_code}m\033[48;2;${background_color}m"
+
+        echo -ne "\033[38;5;${formatting_codes}m"
+    fi
+}
+
+# Reset color styles
+resetStyle() {
+    # If $NO_COLOR not defined or empty
+    if [[ ! -v $NO_COLOR || -z "${!NO_COLOR}" ]]; then
+        echo -ne "\033[0m"
+    fi
+}
+
+# Apply a style by style name
+## Run: applyStyleByName "sea green" "vivid light magenta" "bold" "dim" "blink"
+applyStyleByName() {
+    # If $NO_COLOR not defined or empty
+    if [[ ! -v $NO_COLOR || -z "${!NO_COLOR}" ]]; then
+        
+        # colors
+        local foreground_name="$1"
+        local background_name="$2"
+
+        # Remove $1 and $2
+        shift 2
+
+        # formatting styles
+        local formatting_names=("$@")
+
+        local foreground_code=$(getColorByName "$foreground_name")
+        local background_code=$(getColorByName "$background_name")
+        local formatting_codes=$(getFormattingCodeByNames "${formatting_names[@]}")
+        
+        echo -ne "\033[38;5;${foreground_code}m\033[48;5;${background_code}m"
+        echo -ne "\033[${formatting_codes}m"
+    fi
+}
+
+applyStyleByCodes "212" "122" "1" 
+echo -n " RGB "
+resetStyle
+echo ""
+
+applyStyleByName "sea green" "vivid light magenta" "bold" "dim" 
+echo -ne " sea green - vivid light magenta - bold dim "
+resetStyle
+echo ""
+
+TERM="xterm-color"
+applyStyleByCodes "30" "46" "1" "2" 
+echo -n " ANSI black - background cyan - bold dim"
+resetStyle
+echo ""
+
+applyStyleByName "green" "background cyan" "bold" "dim" 
+echo -ne " ANSI green - background cyan - bold dim "
+resetStyle
+echo ""
+
+
+applyStyleByName "bright green" "bright background cyan" "bold" "dim" 
+echo -ne " ANSI bright green - bright background cyan - bold dim "
+resetStyle
+echo ""
+
+
+
+# Funzione per stampare il testo con formattazioni multiple
+print_colored_text() {
+    local background_code="$1"  # Codice di sfondo (da 0 a 7 o da 0 a 255)
+    local foreground_code="$2" 
+    local format_codes="$3"  # Accetta più codici di formattazione
+    local message="$4"
+    
+    # Creazione della sequenza di escape
+    local color_code="\033[${background_code}m \033[${foreground_code}m \033[${format_codes}m"
+    local reset_code="\033[0m"  # Reset
+
+    # Stampa il messaggio
+    # echo -e "${color_code}${message}${reset_code}"
+
+    echo -e "\033[48;2;0;0;255m\033[38;5;116m${message}${reset_code}"
+}
+
+# Esempio di utilizzo
+# print_colored_text "32" "44" "1" "grassetto e sottolineato bianco su sfondo blu"
+print_colored_text "48;5;88" "38;5;176" "1;2" " sottolineato e soprascritto nero su sfondo rosso"
+
