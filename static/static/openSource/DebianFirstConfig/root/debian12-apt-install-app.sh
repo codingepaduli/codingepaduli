ce264915d7a60428662f3da568afd6b9f93f2c4d
codingepaduli <padulodeveloper@protonmail.com>
2026-04-10 19:42:49 +0200

    OpenSource - Updated post about first Debian GNU/Linux configuration - updated installed software

--- static/static/openSource/DebianFirstConfig/root/debian12-apt-install-app.sh
+++ static/static/openSource/DebianFirstConfig/root/debian12-apt-install-app.sh
@@ -250,8 +250,9 @@ fi
 
 ## tealdeer : gestisce le pagine TLDR
 ## screen: gestisce le sessioni di terminale, quando la connessione cade o si chiude
+## tmux:   gestisce le sessioni di terminale, quando la connessione cade o si chiude
 ##         involontariamente un terminale, ci si può ricollegare e riprendere.
-apt-get install $APT_OPTS bash bash-completion curl dash diffutils tealdeer screen
+apt-get install $APT_OPTS bash bash-completion curl dash diffutils tealdeer screen tmux
 
 if [ $? -ne 0 ]; then
     exit 1
